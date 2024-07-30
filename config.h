19efe29dbc761eb8bcd563a93f8258030d3e8c7a
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2024-07-30 06:58:52 +0530

    updated config

--- config.h
+++ config.h
@@ -1,32 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xf0;
+static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xf2;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-  "NotoColorEmoji:size=10:antialias=true:autohint=true"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+    "JetBrainsMono Nerd Font:size=10:antialias=true:autohint=true",
+    "NotoColorEmoji:size=13:antialias=true:autohint=true"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#fbf1c7", "#1d2021" },
-	[SchemeSel] = { "#b8bb26", "#282828" },
-	[SchemeOut] = { "#fbf1c7", "#8ec07c" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ebdbb2", "#282828"},
+    [SchemeSel] = {"#282828", "#ebdbb2"},
+    [SchemeOut] = {"#ebdbb2", "#8ec07c"},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+    [SchemeNorm] = {OPAQUE, alpha},
+    [SchemeSel] = {OPAQUE, alpha},
+    [SchemeOut] = {OPAQUE, alpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines = 1;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
+static unsigned int lineheight = 40;
+static unsigned int min_lineheight = 15;
 
 /*
  * Characters not considered part of a word while deleting words
