6dee018a255c388ac9130965fb2ca1a7409a58a3
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2026-05-12 20:42:33 +0530

    font update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xf2;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "JetBrainsMono Nerd Font:size=10:antialias=true:autohint=true",
+    "DepartureMono Nerd Font:size=10:antialias=true:autohint=true",
     "NotoColorEmoji:size=13:antialias=true:autohint=true"};
 static const char *prompt =
     "⚡ Launch:"; /* -p  option; prompt to the left of input field */
